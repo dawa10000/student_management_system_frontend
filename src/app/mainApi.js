3e4b197fb0fd685acb1b35e34319ff1eb0b2d8c3
Dawa Lama <https://github.com/dawa10000>
2026-03-20 12:04:08 +0545

    changes

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,7 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseUrl = 'http://localhost:5000/api';
-export const base = 'http://localhost:5000'
+// export const baseUrl = 'http://localhost:5000/api';
+// export const base = 'http://localhost:5000'
+
+
+export const baseUrl = 'https://student-mgmt-system-backend.onrender.com//api';
+export const base = 'https://student-mgmt-system-backend.onrender.com/'
 
 export const mainApi = createApi({
   reducerPath: 'mainApi',

fcc60082f1040ac81f244f60b0a4e9a7192832f5
Dawa Lama <https://github.com/dawa10000>
2026-03-20 12:22:18 +0545

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import RootLayout from "./components/RootLayout.jsx";
 import Register from "./features/auth/Register.jsx";
-import Login from "./features/auth/Login.jsx";
+import Login from "./features/auth/Login.jsx"; npm
 import UserProfile from "./features/user/UserProfile.jsx";
 import Dashboard from "./features/addstudent/Dashboard.jsx";
 import StudentEditForm from "./features/addstudent/StudentEditForm.jsx";
